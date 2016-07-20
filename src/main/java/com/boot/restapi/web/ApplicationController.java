79932b9eb0543b6a2369f5088fc0000f12fb2f19
Oleg <oleg.bezkorovajniy@gmail.com>
2016-07-21 01:14:57 +0300

    update

--- src/main/java/com/boot/restapi/web/ApplicationController.java
+++ src/main/java/com/boot/restapi/web/ApplicationController.java
@@ -1,19 +1,50 @@
 package com.boot.restapi.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import com.boot.restapi.dao.DataObjectRepository;
+import com.boot.restapi.model.DataObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Created by Oleg on 20 Jul 2016.
  */
 
-@Controller
+@RestController
+@RequestMapping(value = "/data")
 public class ApplicationController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @Autowired
+    private DataObjectRepository dataObjectRepository;
 
-    public String homepage() {
-        return "index";
+    @Transactional
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<DataObject> getData() {
+        return dataObjectRepository.findAll();
     }
+
+    @Transactional
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public DataObject getDataById(@PathVariable("id") int id) {
+        return dataObjectRepository.findOne(id);
+    }
+
+    @Transactional
+    @RequestMapping(value = "/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public DataObject addTransactionLog(@RequestBody DataObject data) {
+        return dataObjectRepository.save(data);
+    }
+
+    @Transactional
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public void deleteTransactionsLog(@PathVariable("id") int id) {
+        dataObjectRepository.delete(id);
+    }
+
+
 }
